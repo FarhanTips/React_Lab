9efa6694f23a07ffcab0162870c8f986fb4dfc4e
FarhanTips <farhanakbor02@gmail.com>
2026-09-02 12:32:13 +0600

    Basic CSS styling inside component

--- 1. React Core/my-first-react-app/src/App.tsx
+++ 1. React Core/my-first-react-app/src/App.tsx
@@ -7,13 +7,20 @@ function App() {
     <>
     <h1>Get Started</h1>
     <Student></Student>
+    <Student></Student>
+    <Student></Student>
     </>
   )
 }
 
 function Student() {
+  const studentStyle = {
+    border: "2px solid salmon",
+    borderRadius: "10px",
+    margin: "10px"
+  }
   return (
-    <div>
+    <div style={studentStyle}>
       <h3>Name:</h3>
       <p>Grades:</p>
     </div>
