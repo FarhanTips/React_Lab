d44bcba2ed6a89c77801b56caf4927376cd99c58
FarhanTips <farhanakbor02@gmail.com>
2026-09-06 01:45:26 +0600

    Posts data fetch from APP file

--- 2. React State management, events and Data fetching/react-state-more/src/App.tsx
+++ 2. React State management, events and Data fetching/react-state-more/src/App.tsx
@@ -1,4 +1,4 @@
-
+import { use } from "react";
 import { Suspense } from 'react';
 import './App.css'
 import Posts from './posts data fetch';
@@ -9,6 +9,13 @@ const postsPromise = async ()=> {
   return data;
 }
 
+const helloPromise = async ()=> {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+  return data;
+}
+
+
 function App() {
 
   return (
@@ -16,8 +23,23 @@ function App() {
     <Suspense fallback={<p>Please wait...</p>}>
     <Posts props = {postsPromise()}></Posts>
     </Suspense>
+
+    <Suspense fallback={<p>Please wait...</p>}>
+    <Hello props = {helloPromise()}></Hello>
+    </Suspense>
     </>
   )
 }
+function Hello({props}){
+  // যে function-টাকে JSX-এ React component হিসেবে ব্যবহার করবে, JSX-এ তার নাম Capital letter দিয়ে লিখতে হবে।
+    const posts = use(props);
+    return (
+      <div>
 
+        {
+          posts.map(elem=> <p>title: {elem.title}</p>)
+        }
+      </div>
+    )
+}
 export default App
