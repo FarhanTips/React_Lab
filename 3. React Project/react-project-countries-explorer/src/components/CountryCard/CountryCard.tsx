48086ba48715db0b46540cee3d8061e540a96e48
FarhanTips <farhanakbor02@gmail.com>
2026-09-06 22:33:29 +0600

    Conditional CSS classes and toggle state on user click

--- 3. React Project/react-project-countries-explorer/src/components/CountryCard/CountryCard.tsx
+++ 3. React Project/react-project-countries-explorer/src/components/CountryCard/CountryCard.tsx
@@ -1,13 +1,20 @@
+import { useState } from "react";
 import type { CountryType } from "../../type";
 import "./countrycard.css"
 
 export function CountryCard({country}: {country: CountryType}){
+
+    const [visited, SetVisited] = useState(false);
+    const handleVisited = ()=>{
+        SetVisited(!visited);
+    }
     return (
-        <div className="countryCard">
+        <div className={`countryCard ${visited ? 'country-visited' : ''}`}>
             <h3>{country.name.common}</h3>
             <img src={country.flags.flags.png} alt="country.flags.flags.alt" />
             <p>Population: {country.population.population}</p>
             <p>Capital: {country.capital.capital}</p>
+            <button onClick={handleVisited}>{visited ? "Visited" : "Mars as Visited"}</button>
         </div>
     )
 }
