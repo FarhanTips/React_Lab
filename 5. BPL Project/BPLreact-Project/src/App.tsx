270711784608c82158a0b33b8f3d8f64c45070c6
FarhanTips <farhanakbor02@gmail.com>
2026-09-09 21:59:53 +0600

    synthetic data of players

---
+++ 5. BPL Project/BPLreact-Project/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h2 className='text-5xl text-amber-600'>Hello</h2>
+    <button className="btn btn-soft btn-success">Success</button>
+    </>
+  )
+}
+
+export default App
